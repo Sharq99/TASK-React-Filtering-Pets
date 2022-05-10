879016f3f056dadf309047bb746fd454be70e55d
AB <aasharq99@gmail.com>
2022-05-10 19:44:19 +0300

    EDITTED PetList and PetItem

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,14 +1,22 @@
-function PetItem({ pet }) {
+import React, { useState } from 'react';
+
+function PetItem({ pet, handleAdopt }) {
+  const [petimage, setPetImage] = useState(pet.image);
+  
+  const turnGIF = () => {
+    setPetImage(pet.image2);
+  } 
+
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={petimage} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
+          <button type="button" onClick={turnGIF} className="btn btn-info">
             Pet
           </button>
-          <button type="button" class="btn btn-info  m-2">
+          <button type="button" onClick={() => handleAdopt(pet.id)} class="btn btn-info  m-2">
             Adopt
           </button>
         </div>
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,8 +1,111 @@
-import pets from "../petsData";
+// import petsData from "../petsData";
+// import PetItem from "./PetItem";
+// import React, { useState } from 'react';
+
+// function PetsList() {
+//   const [pets, setPets] = useState(petsData);
+  
+//   const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+
+//   const [query, setQuery] = useState("");
+//   const [type, setType] = useState("");
+
+//   let searchPet = (event) => {
+//     let petLowerCase = event.target.value.toLowerCase();
+//     setQuery(petLowerCase);
+
+//     let qPets = pets.filter(P => P.type.toLowerCase().includes(query));
+//     //qPets = 
+//   }
+
+//   let selectPet = (event) => {
+//     setType(event);
+
+//     let fPets = pets.filter((P) => P.type === type);
+//     console.log(fPets);
+//   }
+
+//   let handleAdopt = (adoptID) => {
+//     let index = pets.findIndex(x => x.id === adoptID);
+//     pets.splice(index, 1);
+//   }
+
+//   return (
+//     <section id="doctors" className="doctor-section pt-140">
+//       <div className="container">
+//         <div className="row justify-content-center">
+//           <div className="col-xxl-5 col-xl-6 col-lg-7">
+//             <div className="section-title text-center mb-30">
+//               <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
+//                 Fur-ends
+//               </h1>
+//               <div className="input-group rounded">
+//                 <input
+//                   type="search"
+//                   onChange = {searchPet}
+//                   className="form-control rounded"
+//                   placeholder="Search"
+//                   aria-label="Search"
+//                   aria-describedby="search-addon"
+//                 />
+//               </div>
+//               <br />
+//               Type:
+//               <select className="form-select" onChange = {event => selectPet(event.target.value)}>
+//                 <option value="" selected>
+//                   All
+//                 </option>
+//                 <option value="Cat">Cat</option>
+//                 <option value="Dog">Dog</option>
+//                 <option value="Rabbit">Rabbit</option>
+//               </select>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="row justify-content-center">{petList}</div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default PetsList;
+
+
+import petsData from "../petsData";
 import PetItem from "./PetItem";
+import React, { useState } from 'react';
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  const [pets, setPets] = useState(petsData);
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState("");
+
+  const handleAdopt = (adoptID) => setPets(pets.filter((pet) => pet.id !== adoptID));
+
+  const qPets = pets.filter((pet) => pet.name.toLowerCase().includes(query.toLowerCase()) && pet.type.includes(type)).map((pet) => <PetItem pet={pet} key={pet.id} handleAdopt={handleAdopt} />);
+  
+  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} handleAdopt={handleAdopt}/>);
+
+  
+  
+
+  let searchPet = (event) => {
+    // let petLowerCase = event.target.value.toLowerCase();
+    // setQuery(petLowerCase);
+    setQuery(event.target.value);
+
+    //let qPets = pets.filter(P => P.type.toLowerCase().includes(query)); 
+  }
+
+  let selectPet = (event) => {
+    setType(event);
+
+    let fPets = pets.filter((P) => P.type === type);
+    console.log(fPets);
+  }
+
+  
 
   return (
     <section id="doctors" className="doctor-section pt-140">
@@ -16,6 +119,7 @@ function PetsList() {
               <div className="input-group rounded">
                 <input
                   type="search"
+                  onChange = {searchPet}
                   className="form-control rounded"
                   placeholder="Search"
                   aria-label="Search"
@@ -24,7 +128,7 @@ function PetsList() {
               </div>
               <br />
               Type:
-              <select className="form-select">
+              <select className="form-select" onChange = {event => selectPet(event.target.value)}>
                 <option value="" selected>
                   All
                 </option>
@@ -36,7 +140,7 @@ function PetsList() {
           </div>
         </div>
 
-        <div className="row justify-content-center">{petList}</div>
+        <div className="row justify-content-center">{qPets}</div>
       </div>
     </section>
   );
